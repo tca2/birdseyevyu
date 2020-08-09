ea4ed6ae11339e7400a2eaabfcea6f733b990d9f
Joshua Rosenberg <jmichaelrosenberg@gmail.com>
2020-08-09 14:21:43 -0400

    quick stash before trying to work by file

--- R/1-prep-time-series.R
+++ R/1-prep-time-series.R
@@ -8,7 +8,7 @@
 #' @examples
 #'
 
-prep_time_series <- function(column, specified_file, directory, code = NULL, round = "s") {
+prep_time_series <- function(column, specified_file = NULL, directory, code = NULL, round = "s") {
 
   # argument check
   if (is.null(column)) {
@@ -20,9 +20,6 @@ prep_time_series <- function(column, specified_file, directory, code = NULL, rou
     tibble::as_tibble() %>%
     janitor::clean_names()
 
-  df_of_codes <- df_of_codes %>%
-    dplyr::filter(file == specified_file)
-
   # this is if folks do not provide a code name
   if (is.null(code)) {
     code <- names(df_of_codes)[stringr::str_detect(names(df_of_codes), "code01")]
@@ -31,37 +28,45 @@ prep_time_series <- function(column, specified_file, directory, code = NULL, rou
     }
   }
 
-  # need to fix all of the object names below
-  df_of_codes <- df_of_codes %>%
-    select(all_of(code), contains("onset"), contains("offset")) %>%
-    set_names(c("code", "onset", "offset"))
+  # all of this is for a single file
+  if (!is.null(specified_file)) {
+    df_of_codes <- df_of_codes %>%
+      dplyr::filter(file == specified_file)
 
-  if (round == "s") {
-    d <- df_of_codes %>%
-      mutate(onset = round(onset / 1000), # allow manual specification of rounding
-             offset = round(offset / 1000))
-  } else if (round == "m") {
-    d <- df_of_codes %>%
-      mutate(onset = round(onset / (1000 * 60)), # allow manual specification of rounding
-             offset = round(offset / (1000 * 60)))
-  } else if (round == "ms") {
-    d <- df_of_codes
+
+    # need to fix all of the object names below
+    df_of_codes <- df_of_codes %>%
+      select(all_of(code), contains("onset"), contains("offset")) %>%
+      set_names(c("code", "onset", "offset"))
+
+    if (round == "s") {
+      d <- df_of_codes %>%
+        mutate(onset = round(onset / 1000), # allow manual specification of rounding
+               offset = round(offset / 1000))
+    } else if (round == "m") {
+      d <- df_of_codes %>%
+        mutate(onset = round(onset / (1000 * 60)), # allow manual specification of rounding
+               offset = round(offset / (1000 * 60)))
+    } else if (round == "ms") {
+      d <- df_of_codes
+    }
+
+    dd <- map2(d$onset, d$offset, time_seq)
+
+    dd <- tibble(ts = unlist(dd))
+
+    d <- d %>%
+      pivot_longer(cols = c("onset", "offset")) %>%
+      rename(ts = value)
+
+    ddd <- left_join(dd, d, by = "ts") %>%
+      fill(code) %>%
+      select(-name)
+
   }
-
-  dd <- map2(d$onset, d$offset, time_seq)
-
-  dd <- tibble(ts = unlist(dd))
-
-  d <- d %>%
-    pivot_longer(cols = c("onset", "offset")) %>%
-    rename(ts = value)
-
-  ddd <- left_join(dd, d, by = "ts") %>%
-    fill(code)
 
   attributes(ddd)$round <- round
 
   ddd
-
 }
 
